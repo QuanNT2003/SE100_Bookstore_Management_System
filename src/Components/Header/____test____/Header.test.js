61f787d9139d85430ec13a856ed3b4e3aaba51cf
Quan Ngo <ngotrungquan1412@gmail.com>
2023-11-09 21:28:36 +0700

    TestHeader

--- src/Components/Header/____test____/Header.test.js
+++ src/Components/Header/____test____/Header.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 import { BrowserRouter } from 'react-router-dom';
-test('renders learn react link', () => {
+test('renders header and find title', () => {
     const header =
     {
         id: 1,
@@ -15,5 +15,23 @@ test('renders learn react link', () => {
         </BrowserRouter>
     );
     const linkElement = screen.getByText(header.title);
+    expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders header and find name', () => {
+    const header =
+    {
+        id: 1,
+        title: "Trang sản phẩm",
+        img: "https://upload.wikimedia.org/wikipedia/vi/1/1d/Manchester_City_FC_logo.svg",
+        name: "Pham Tuan Kiet",
+    }
+    render(
+        <BrowserRouter>
+            <Header info={header} />
+        </BrowserRouter>
+    );
+    const linkElement = screen.getByText(header.name);
     expect(linkElement).toBeInTheDocument();
 });
